a833ba1810ee223e53fa4b1d90b591fb3cd6d198
Ed Phillips <ed@theteflacademy.com>
2021-09-24 22:49:20 +0700

    Version bump

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021090101;
+$plugin->version   = 2021092400;
 $plugin->requires  = 2017051501;
 $plugin->component = 'local_teflacademyreceivecrmcodes';
